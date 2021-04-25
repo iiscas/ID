266c44dafcc2a5090ce7048a254b3ec1c55ec5f8
Isabel Castro <isabelcastro4@outlook.pt>
2021-04-25 22:35:10 +0100

    classes criadas

--- a21280377_IsabelCastro_a21280450_VivianaMoreira_P2/src/Jogador.java
+++ a21280377_IsabelCastro_a21280450_VivianaMoreira_P2/src/Jogador.java
@@ -8,14 +8,6 @@
  *
  * @author isabe
  */
-public class main {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.Println();
-    }
+public class Jogador {
     
 }
