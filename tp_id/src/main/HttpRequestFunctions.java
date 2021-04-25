64359963afad4c4e097cc8756e629ac113aed360
Isabel Castro <isabelcastro4@outlook.pt>
2021-04-25 22:52:06 +0100

    alone now

--- tp_id/src/main/HttpRequestFunctions.java
+++ tp_id/src/main/HttpRequestFunctions.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package tp;
+package main;
 import java.io.*;
 import java.net.MalformedURLException;
 import java.net.URL;
--- tp_id/src/main/Tp_id.java
+++ tp_id/src/main/Tp_id.java
@@ -3,11 +3,19 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package main;
 
 /**
  *
  * @author isabe
  */
-public class Jogador {
+public class Tp_id {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
     
 }
